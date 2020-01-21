911aaa1d0e4e4f238c9f1cea914a6422ae249b20
kp1129 <katya.p.1129@gmail.com>
2020-01-20 19:06:02 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-//TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [lionsScore, setLionsScore] = useState(0);
   const [tigersScore, setTigersScore] = useState(0);
 
@@ -20,9 +18,6 @@ function App() {
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
             <div className="home__score">{lionsScore}</div>
           </div>
           <div className="timer">00:03</div>
@@ -35,7 +30,6 @@ function App() {
       </section>
       <section className="buttons">
         <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={lionsTouchdown}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={lionsFieldgoal}>Home Field Goal</button>
         </div>
